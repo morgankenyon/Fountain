57ffd20e149658d556a18798eb209cba501c4fc7
Morgan Kenyon <morganwkenyon@gmail.com>
2023-07-30 14:19:34 -0500

    Working on listing local migrations

--- src/Fountain.Migrations/Program.fs
+++ src/Fountain.Migrations/Program.fs
@@ -1,4 +1,5 @@
 ﻿open Fountain.Migrations.Runner
+open Fountain.Migrations.Db
 
 open System.IO
 open System
@@ -10,11 +11,17 @@ type CliError =
 
 type FountainArgs =
     | [<AltCommandLine("-p")>] Ping
+    | [<AltCommandLine("-v")>] Verify
+    | [<AltCommandLine("-ll")>] LocalMigrations
+    //| [<AltCommandLine("-dbm")>] DbMigrations
 with
     interface IArgParserTemplate with
         member this.Usage =
             match this with
             | Ping -> "Can you reach the db???"
+            | Verify -> "Verify that you're connecting to a Fountain Database"
+            | LocalMigrations -> "List local migrations"
+            //| DbMigration -> "List db migrations"
 
 let getExitCode result =
     match result with
@@ -30,12 +37,6 @@ let runPrint print =
 [<EntryPoint>]
 let main argv =
     let connectionString = "Host=localhost; Database=fountain; Username=app; Password=fountain_cli_365;"
-    
-    //while true do
-
-    //    match canPingDb(connectionString) with
-    //    | true -> printfn "Can reach db" 
-    //    | false -> printfn "Cannot reach db"
 
     let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some ConsoleColor.Red)
     let parser = ArgumentParser.Create<FountainArgs>(programName = "fount", errorHandler = errorHandler)
@@ -45,6 +46,16 @@ let main argv =
         match canPingDb(connectionString) with
         | true -> runPrint "Can reach db"
         | false -> runPrint "Cannot reach db"
+    | p when p.Contains(Verify) ->
+        match checkIfFountainTableExists(connectionString) with
+        | true -> runPrint "Connecting to a Fountain db"
+        | false -> runPrint "Database is not a Fountain db"
+    | p when p.Contains(LocalMigrations) ->
+        let migrations = getLocalMigrations()
+        for mig in migrations do
+            let name = mig.GetInfo().Name
+            printfn "%s" name
+        Ok ()
     | _ ->
         printfn "%s" (parser.PrintUsage())
         Error ArgumentsNotSpecified
--- src/Fountain.Migrations/Tool.fs
+++ src/Fountain.Migrations/Tool.fs
@@ -1,12 +1,21 @@
 ﻿namespace Fountain.Migrations
 
 module Tool =
+    open System
 
     type MigrationInfo =
         {
             Number : int
             Name : string
             Description : string
+        }
+
+    type MigrationRecord =
+        {
+            MigrationId : int
+            Name : string
+            Description : string
+            DateApplied : DateTime
         }
 
     type IMigration =
@@ -32,18 +41,81 @@ module Runner =
                     isUp = true
                 })
         not checkInfo.IsEmpty
+    
+    (*
+        SELECT EXISTS (
+        SELECT 1
+        FROM pg_tables
+        WHERE tablename = 'emp_details'
+        ) AS table_existence;
+    *)
+    let checkIfFountainTableExists (connectionString: string) =
+        let query = "SELECT EXISTS (
+        SELECT 1
+        FROM pg_tables
+        WHERE tablename = 'dbo.fount'
+        ) AS table_exists;"
+
+        connectionString
+        |> Sql.connect
+        |> Sql.query query
+        |> Sql.executeRow (fun read ->
+            read.bool "table_exists")
 
 module Db =
     open Tool
+    open System
 
-    type InitialMigration() =
+    let getLocalMigrations() =
+        let migType = typedefof<IMigration>
+        let localMigrations = 
+            AppDomain.CurrentDomain.GetAssemblies()
+            |> Array.collect (fun a -> a.GetTypes())
+            |> Array.filter (fun t -> migType.IsAssignableFrom(t) && not (t.IsInterface))
+            |> Array.map (fun mig -> (Activator.CreateInstance mig) :?> IMigration)
+            |> Array.sortBy (fun mig -> mig.GetInfo())
+        localMigrations
+
+    type InitializeFountain() =
         interface IMigration with
             member this.GetInfo() =
                 {
                     Number = 0
-                    Name = "Initial"
-                    Description = "Initial Migration, creating Posts table"
+                    Name = "InitializeFountain"
+                    Description = "Creating tracking table for Fountain"
                 }
+            member this.Up() = "
+                CREATE TABLE dbo.fount(
+                    ID              SERIAL PRIMARY KEY,
+                    NAME            TEXT NOT NULL,
+                    DESCRIPTION     TEXT NOT NULL,
+                    DATEAPPLIED     TIMESTAMP WITH TIME ZONE NOT NULL
+                );"
+            //will need to prompt user (use a special flag) to remove this migration
+            //since you lose tracking at that point
+            member this.Down() = "DROP TABLE dbo.fount"
 
+    type TestMigration() =
+        interface IMigration with
+            member this.GetInfo() =
+                {
+                    Number = 1
+                    Name = "TestMigration"
+                    Description = "Testing migration"
+                }
             member this.Up() = ""
+            //will need to prompt user (use a special flag) to remove this migration
+            //since you lose tracking at that point
             member this.Down() = ""
+
+    // type InitialMigration() =
+    //     interface IMigration with
+    //         member this.GetInfo() =
+    //             {
+    //                 Number = 0
+    //                 Name = "Initial"
+    //                 Description = "Initial Migration, creating Posts table"
+    //             }
+
+    //         member this.Up() = ""
+    //         member this.Down() = ""
